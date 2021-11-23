5ce245a5882b42ef30ea546c2754a98e0c439642
Sébastien Chopin <seb@nuxtjs.com>
2021-11-23 03:46:37 +0100

    chore: update design padding

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@ const { user } = useGitHub()
 </script>
 
 <template>
-  <div class="container mx-auto md:mt-20 p-4 sm:p-10 md:px-20 shadow bg-light-50 flex flex-col gap-5">
+  <div class="container mx-auto md:my-20 p-4 sm:p-10 md:px-20 shadow bg-light-50 flex flex-col gap-5">
     <h1 class="text-4xl flex items-center gap-2">Discuss with Atinux <i class="inline-flex i-carbon-chat text-black"/></h1>
     <User />
     <NewMessage v-if="user" />
